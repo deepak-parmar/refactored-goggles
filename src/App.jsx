9b14516005d21a752aabfff3cbd5703f93ed44f8
Deepak Parmar <dgparmar1999@rediffmail.com>
2023-04-05 21:02:54 +0530

    Loose base constant

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,6 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Contact from "./components/Contact";
 import Layout from "./components/Layout";
 import Work from "./components/Work";
-const base = "/refactored-goggles/";
 
 export default function App() {
   return (
